61590e690ef04123272daaead9f261851ce6a9e2
Hong Junho <102346636+Hollolloho@users.noreply.github.com>
2026-09-24 23:54:11 +0900

    feat. move함수 정의

--- Labs/LEC06/character_draws.py
+++ Labs/LEC06/character_draws.py
@@ -2,6 +2,16 @@
 
 from pico2d import *
 
-while True:
+def move_circle():
     pass
+def move_rectangle():
+    pass
+def move_triangle():
+    pass
+while True:
+    move_circle()
+    move_rectangle()
+    move_triangle()
+    pass
+
 
