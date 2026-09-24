5a91984eb663e0508a0b5a2ae760cac4e4af0bc3
Hong Junho <102346636+Hollolloho@users.noreply.github.com>
2026-09-25 00:20:16 +0900

    feat.캐릭터 그리기 기능 추가

--- Labs/LEC06/character_draws.py
+++ Labs/LEC06/character_draws.py
@@ -7,20 +7,29 @@ open_canvas(1200, 800)
 character = load_image('character.png')
 
 def move_circle():
+    clear_canvas()
+    character.draw(600, 400)
+    update_canvas()
     print("CIRCLE")
     pass
+
 def move_rectangle():
+    clear_canvas()
+    character.draw(600, 400)
+    update_canvas()
     print("RECTANGLE")
     pass
+
 def move_triangle():
+    clear_canvas()
+    character.draw(600, 400)
+    update_canvas()
     print("TRIANGLE")
-    pass
+    pass   
 
 while True:
     move_circle()
     move_rectangle()
     move_triangle()
-    pass
-
 
 close_canvas()
